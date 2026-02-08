df30bee353bb1e5b391e1bbbac7e8977e8bad6ab
Muhammed Korkmaz <muhammed13@gmx.de>
2026-02-08 19:03:32 +0100

    - clearList can now delete one element

--- index.js
+++ index.js
@@ -1,21 +1,14 @@
 function createTask() {
   var list = document.getElementById("listContent");
-
-  // Just pasted!!!
-  // Source - https://stackoverflow.com/a/17774064
-  // Posted by Felix Kling, modified by community. See post 'Timeline' for change history
-  // Retrieved 2026-02-07, License - CC BY-SA 3.0
-
-  var firstname = document.getElementById("listContent").value;
   var entry = document.createElement("li");
   entry.appendChild(document.createTextNode("listContent"));
   list.appendChild(entry);
 }
 
 function clearList() {
-  var firstname = document.getElementById("listContent").value;
-  firstname.value.
+  var list = document.getElementById("listContent");
+  if (list.hasChildNodes()) {
+    list.removeChild(list.children[0]);
+    console.log(list.children.length);
+  }
 }
-/*
-F12 for debugging in web
- */
